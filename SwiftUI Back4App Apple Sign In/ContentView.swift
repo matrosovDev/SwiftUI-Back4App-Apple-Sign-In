a561ae9627d5cd8d50e18240940e9dd8d5d99e52
Oleksandr Matrosov <matrosov.developer@gmail.com>
2024-11-26 16:58:24 +0100

    Initial Commit

---
+++ SwiftUI Back4App Apple Sign In/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI Back4App Apple Sign In
+//
+//  Created by Oleksandr Matrosov on 26/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI Back4App Apple Sign In/SwiftUI_Back4App_Apple_Sign_InApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Back4App_Apple_Sign_InApp.swift
+//  SwiftUI Back4App Apple Sign In
+//
+//  Created by Oleksandr Matrosov on 26/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Back4App_Apple_Sign_InApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
